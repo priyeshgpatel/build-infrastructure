dbf0f3e686987a420cd055306b0e5f60e1fee4c1
David Kowis <david_kowis@homedepot.com>
2015-07-31 16:46:08 -0500

    Can't use PPA, have to manually magic this

--- myModules/base/manifests/java.pp
+++ myModules/base/manifests/java.pp
@@ -3,10 +3,19 @@ class base::java {
 # Super handy: http://www.webupd8.org/2014/03/how-to-install-oracle-java-8-in-debian.html
 
   include apt
-  apt::key{ 'webupd8key':
-    id     => 'EEA14886',
-    server => 'keyserver.ubuntu.com',
-  }
 
-  apt::ppa{ 'ppa:webupd8team/java': }
+  apt::source { 'webupd8Java':
+    comment => 'webupd8 Oracle Java repository',
+    location => 'http://ppa.launchpad.net/webupd8team/java/ubuntu',
+    release  => 'trusty',
+    repos    => 'main',
+    key      => {
+      'id'     => 'EEA14886',
+      'server' => 'keyserver.ubuntu.com',
+    },
+    include  => {
+      'src' => true,
+      'deb' => true,
+    },
+  }
 }
