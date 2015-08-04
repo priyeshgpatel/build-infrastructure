db4a9619725582d9d2f5282ddcb45bd7f58f28ab
David Kowis <david_kowis@homedepot.com>
2015-08-04 13:04:21 -0500

    Adding self-signed root CA, and wildcard cert/key

--- myModules/ssl_cert/manifests/init.pp
+++ myModules/ssl_cert/manifests/init.pp
@@ -1,3 +1,20 @@
-class ssl_cert{
-  #TODO: when we get SSL certs, stick them in here, however we want to.
+class ssl_cert(
+  $ca_content = undef,
+  $wildcard_cert = undef,
+  $wildcard_key = undef
+){
+
+# add the root CA
+  ssl_cert::certificate{ 'rootCA':
+    cert_name    => "hdTechLabsRoot",
+    cert_content => $ca_content,
+  }
+
+# add our wildcard key/cert for gettin bizness done
+  ssl_cert::certificate{ 'wildcardCert':
+    cert_name    => "build.gc.hdtechlabs.com",
+    cert_content => $wildcard_cert,
+    key_content  => $wildcard_key,
+  }
+
 }
