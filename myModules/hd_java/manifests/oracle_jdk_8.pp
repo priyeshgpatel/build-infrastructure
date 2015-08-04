4b92bcb764cf029bb326ac7b90f7e5661bf06683
David Kowis <david_kowis@homedepot.com>
2015-08-04 14:59:05 -0500

    Getting the license acceptance in there

--- myModules/hd_java/manifests/oracle_jdk_8.pp
+++ myModules/hd_java/manifests/oracle_jdk_8.pp
@@ -7,8 +7,8 @@ class hd_java::oracle_jdk_8 {
   }
 
   exec{ 'java8-license-accept':
-    refreshonly => true,
     command     => "/bin/echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections",
     before      => Package['oracle-java8'],
+    unless      => '/usr/bin/debconf-get-selections | /bin/egrep "oracle-java8-installer.*shared.*true"'
   }
 }
