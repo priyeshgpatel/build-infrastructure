c67991989f78737f386ad664575dcad59a03a897
David Kowis <david_kowis@homedepot.com>
2015-08-04 14:55:02 -0500

    Have the auto-parts backwards

--- myModules/hd_java/manifests/oracle_jdk_8.pp
+++ myModules/hd_java/manifests/oracle_jdk_8.pp
@@ -4,11 +4,11 @@ class hd_java::oracle_jdk_8 {
   package{ 'oracle-java8':
     name   => 'oracle-java8-installer',
     ensure => present,
-    notify => Exec['java8-license-accept'],
   }
 
   exec{ 'java8-license-accept':
     refreshonly => true,
-    command     => "/bin/echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections"
+    command     => "/bin/echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections",
+    before      => Package['oracle-java8'],
   }
 }
