f1896a698a3a3783925fb2b4bc1fd1d817d541cf
David Kowis <david_kowis@homedepot.com>
2015-08-05 10:57:41 -0500

    Forgot ordering on the dhparams

--- myModules/base/manifests/nginx.pp
+++ myModules/base/manifests/nginx.pp
@@ -18,6 +18,7 @@ class base::nginx {
     path    => ['/bin', '/usr/bin'],
     cwd     => "/etc/nginx",
     creates => "/etc/nginx/dhparam.pem",
+    require => Package['nginx'],
   }
 
 # Don't want the default site running.
