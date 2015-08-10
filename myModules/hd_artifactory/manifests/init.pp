dd6183c8b7d2445b06089c29da38be991ca8ef3a
David Kowis <david_kowis@homedepot.com>
2015-08-10 14:54:47 -0500

    Just a simple module to install artifactory

---
+++ myModules/hd_artifactory/manifests/init.pp
@@ -0,0 +1,6 @@
+class hd_artifactory {
+# have to download and install the zip manually :(
+  include hd_artifactory::package
+
+  # TODO: is that it?
+}
---
+++ myModules/hd_artifactory/manifests/package.pp
@@ -0,0 +1,39 @@
+class hd_artifactory::package(
+  $version = "4.0.1",
+  $base_url = "https://bintray.com/artifact/download/jfrog/artifactory/",
+  $sha1 = '8d11e3735789a2cbf38e45a4e3709c92b86a703e'
+){
+
+  include hd_java::oracle_jdk_8
+
+  archive{ "artifactory-$version":
+    ensure           => present,
+    url              => "${base_url}/jfrog-artifactory-oss-${version}.zip",
+    follow_redirects => true,
+    target           => "/opt",
+    extension        => "zip",
+    checksum         => false,
+    digest_type      => "sha1",
+    digest_string    => $sha1,
+    notify           => Exec['install_service'],
+  }
+
+  file { "artifactory-symlink":
+    path    => '/opt/artifactory',
+    ensure  => link,
+    target  => "/opt/artifactory-oss-${version}",
+    require => Archive["artifactory-${version}"]
+  }
+
+  exec{ 'install_service':
+    refreshonly => true,
+    command     => "/opt/artifactory/bin/installService.sh",
+    require     => Class['hd_java::oracle_jdk_8']
+  }
+
+  service{ 'artifactory':
+    ensure    => running,
+    require   => Exec['install_service'],
+    subscribe => Exec['install_service'],
+  }
+}
