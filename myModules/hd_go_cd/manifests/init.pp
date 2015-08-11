dd18e73e7f1cada47c6976a02c4add1028a2959f
David Kowis <david_kowis@homedepot.com>
2015-08-11 16:01:49 -0500

    Poke firewall holes

--- myModules/hd_go_cd/manifests/init.pp
+++ myModules/hd_go_cd/manifests/init.pp
@@ -2,4 +2,9 @@ class hd_go_cd {
   include hd_go_cd::package
 
   #TODO: install the server somewhere, and the agent somewhere else
+  firewall{ '100 gocd access':
+    port   => [8154, 8153],
+    proto  => tcp,
+    action => accept,
+  }
 }
